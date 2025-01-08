2d35122e3e4c7bf3c1127264d4cefb659e0c2338
AbdelrahmanElzayat <bodyelmajec@gmail.com>
2025-01-09 00:06:24 +0200

    netlify toml

--- src/components/Layouts/MainLayout.jsx
+++ src/components/Layouts/MainLayout.jsx
@@ -1,23 +1,14 @@
-
-
-
 import React from "react";
 import Sidebar from "../sidebar/Sidebar";
 import Header from "../header/Header";
 import { Outlet } from "react-router-dom";
 
-const MainLayout= () => {
+const MainLayout = () => {
   return (
     <div className="flex h-screen">
-      {/* Sidebar */}
       <Sidebar />
-
-      {/* Main Content Area */}
       <div className="flex flex-col flex-1">
-        {/* Header */}
         <Header />
-
-        {/* Page Content */}
         <main className="flex-1 p-4 bg-gray-100 overflow-y-auto">
           <Outlet />
         </main>
